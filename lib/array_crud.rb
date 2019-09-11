9799df8e9051dfbf8cd56aba65851ca63331aa32
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-11 14:31:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = [bruno, annes, arabella]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
